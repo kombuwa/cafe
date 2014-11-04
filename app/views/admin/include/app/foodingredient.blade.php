80aca33cf6da5bbb0ada2e974d6b9ee65923aefe
unknown <WINDOWS7@ISURU.(none)>
2014-11-04 11:13:03 +0530

    angular test

--- app/views/admin/include/app/foodingredient.blade.php
+++ app/views/admin/include/app/foodingredient.blade.php
@@ -1,5 +1,5 @@
  	<script>
-	      myApp.controllers.categoryController = function($scope, $http) {
+	       myApp.controllers.ingredientController = function($scope, $http) {
 	        //Do Awesome Controller Stuff As Usual
 	        $scope.message = 'Food Category!';
 	        /*$scope.categorys = [
@@ -7,37 +7,50 @@
 	        	{ id:2, name :"Juice" },
 	        ];*/
 	        
-	        $http.get('/api/food/categorys').success(function(categorys){
-	        	$scope.categorys = categorys;
+	        $http.get('/api/food/ingredients').success(function(ingredients){
+	        	$scope.ingredients = ingredients;
+	        });
+
+	      };
+	      
+	      myApp.controllers.editController = function($scope, $modalInstance, $http, id) {
+	      	
+	      	$scope.providers = [
+		    {value: 'kitchen', text: 'Kitchen'},
+		    {value: 'bar', text: 'Bar'}
+		  ];
+		
+		 
+	        
+	        $scope.loadCategory = function() {
+			$http.get('/api/food/categorys').success(function(categorys){
+	        		$scope.categorys = categorys;
+	        	});
+	  	};
+	  	
+	  	$scope.getCategory = function() {
+	  		id = $scope.item.fcid;
+	  		$http.get('/api/food/category/'+id+'/').success(function(category){
+	        		$scope.categoryname = category.name;
+	        		//return category.name;
+	        	});
+	      
+	  	}
+  
+	      	$http.get('/api/food/item/'+id+'/').success(function(item){
+	        	$scope.item = item;
+	        	$scope.getCategory();
 	        });
 	        
-	        $scope.addCategory = function() {
-	        	var category = {
-	        		name : $scope.newCategoryName
-	        	};
-	        	$scope.isAdd = 1;
-	        	//$scope.categorys.push(category );
-	        	$http.post('/api/food/category', category).success(function(){
-		        	$http.get('/api/food/categorys').success(function(categorys){
-			        	$scope.categorys = categorys;
-			        	$scope.newCategoryName = "";
-			        	$scope.isAdd = 0;
-			        });
-		        });
-	        };
-	       
-	        $scope.deleteCategory = function(id) {
-
-	        	$http.delete('/api/food/category/'+id+'/').success(function(){
-		        	$http.get('/api/food/categorys').success(function(categorys){
-			        	$scope.categorys = categorys;
-			        	$scope.newCategoryName = "";
-			        	$scope.isAdd = 0;
-			        });
-		        });
-	        };
-	        
-	      };
+	      	$scope.ok = function () {
+	    		$modalInstance.close($scope.item);
+	  	};
+	  	
+	  	$scope.cancel = function () {
+    			$modalInstance.dismiss('cancel');
+  		};
+  		
+	      }
 	
 	      myApp.config.push([function() {
 	        //Do Awesome Config Stuff As Usual
@@ -45,6 +58,9 @@
 	
 	      //Add A Dependency
 	      myApp.dependencies.push('ngResource');
+	      myApp.dependencies.push('truncate');
+	      myApp.dependencies.push('xeditable');
+	      myApp.dependencies.push('ui.bootstrap');
 	
 	      myApp.directives.myDirective = [function() {
 	        return function(scope, element, attrs){
@@ -63,6 +79,8 @@
 	          //Do Awesome Service Stuff As Usual
 	        };
 	      }];
+	      
+	     
 	
 	      //Call This At The Bottom Of Your Layout
 	      //myApp.boot();
