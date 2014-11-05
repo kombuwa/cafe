14f85dc5a6b16b4fc11d26efe45d8a3b08420de0
unknown <WINDOWS7@ISURU.(none)>
2014-11-05 11:09:31 +0530

    update add ingredient popup

--- app/views/admin/fooditem.blade.php
+++ app/views/admin/fooditem.blade.php
@@ -25,7 +25,7 @@
     <div id="wrapper" ng-controller="itemController">
     	
     	
-    	<!-- Dialog -->
+    	<!-- edit Dialog -->
     	<script type="text/ng-template" id="editDialog.html">
         <div class="modal-header">
             <h3 class="modal-title">Edit Item!</h3>
@@ -43,17 +43,27 @@
             <button class="btn btn-warning" ng-click="cancel()">Cancel</button>
         </div>
 	   </script>
+
+       <!-- ing Dialog -->
        <script type="text/ng-template" id="ingDialog.html">
         <div class="modal-header">
-            <h3 class="modal-title">Edit Item!</h3>
+            <h3 class="modal-title">Add Ingredients!</h3>
         </div>
         <div class="modal-body">
             
-            <b>Name:</b> <a href="#" editable-text="item.name">@{{item.name || 'no data'}}</a><br>
-            <b>Category:</b> <a href="#" editable-select="item.fcid" onshow="loadCategory($data)" onaftersave="getCategory()" e-ng-options="category.id as category.name for category in categorys">@{{ categoryname  || 'no data'}}</a><br>
-            <b>Provider:</b> <a href="#" editable-select="item.provider" e-ng-options="p.value as p.text for p in providers">@{{item.provider || 'no data'}}</a><br>
-            <b>Description:</b> <a href="#" editable-textarea="item.description" e-rows="7" e-cols="40">@{{item.description||'no data'|characters:48}}</a><br>
-            <b>Price:</b> <a href="#" editable-text="item.price">@{{item.price || 'no data'}}</a><br>
+            <div class="row show-grid">
+                <form ng-submit="addIngredient()">
+                    <div class="col-xs-12 col-md-8">
+                        <select class="form-control" ng-model="newIngredient" ng-options="ingredient.id as ingredient.name for ingredient in ingredients" >
+                            <option value="">Choose Ingredient</option>
+                        </select>
+                    </div>
+                    <div class="col-xs-6 col-md-4"><button class="btn btn-default" type="submit">Add Ingredient</button>
+                        <i class="fa fa-spinner fa-spin" ng-show="isAdd"></i>
+                    </div>
+                </form>
+            </div>
+
         </div>
         <div class="modal-footer">
             <button class="btn btn-primary" ng-click="ok()">OK</button>
