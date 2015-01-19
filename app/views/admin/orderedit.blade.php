5eeeb30e464b3c8f8f52efad66359065ff35b849
Isuru Udana Palihawadana <isuruudana@gmail.com>
2015-01-19 12:50:34 +0530

    update

--- app/views/admin/orderedit.blade.php
+++ app/views/admin/orderedit.blade.php
@@ -61,7 +61,7 @@
     			                  	<span class="glyphicon glyphicon-minus"></span>
     			              	</button>
     			          </span>
-    			          <input type="text" name="quant[1]" class="form-control input-number" value="@{{item.item}}" min="1" max="3" >
+    			          <input type="text" name="quant[1]" class="form-control input-number" value="@{{orderitem.qty}}" min="1" max="3" >
     			          <span class="input-group-btn">
     			              	<button type="button" class="btn btn-default btn-number" data-type="plus" data-field="quant[1]">
     			                  	<span class="glyphicon glyphicon-plus"></span>
