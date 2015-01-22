bca1ac04e545c5ba6a9c36daad8b085d2d9c9ec1
Isuru Udana Palihawadana <isuruudana@gmail.com>
2015-01-22 16:34:32 +0530

    ang test

--- app/views/admin/include/app/orderedit.blade.php
+++ app/views/admin/include/app/orderedit.blade.php
@@ -59,7 +59,13 @@
 	        };
 
 	        $scope.pItem = function(id) {
-	        	$scope.orderitems[id].qty = $scope.orderitems[id].qty + 1;
+	        	//$scope.orderitems[id].qty = $scope.orderitems[id].qty + 1;
+	        	for(var i = $scope.orderitems.length - 1; i >= 0; i--){
+				    if($scope.orderitems[i].id == id){
+				       $scope.orderitems[i].qty = $scope.orderitems[i].qty +1;
+				    }
+				}
+
 	        	$http.put('/api/order/item/p/'+id+'/').success(function(){
 			        $http.get('/api/order/items/'+searchId+'').success(function(items){
 			        	$scope.orderitems = items;
