de6d7b5502ea32fa22f758919e64a055d091b62b
Isuru Udana Palihawadana <isuruudana@gmail.com>
2015-02-05 16:51:22 +0530

    small update

--- app/views/admin/orderedit.blade.php
+++ app/views/admin/orderedit.blade.php
@@ -80,7 +80,7 @@
 
 			<accordion close-others="oneAtATime">
     
-		    <accordion-group heading="@{{category.name}}" ng-repeat="category in categorys" select="getFoodItem(category.id)">
+		    <accordion-group heading="@{{category.name}}" ng-repeat="category in categorys" ng-click="getFoodItem(category.id)">
 		    	<div ng-repeat="item in items"  ng-click="addItem(item.id, order.id);" >
 						
 						  @{{item.name}}
