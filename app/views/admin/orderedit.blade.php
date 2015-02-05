03607c35a3a78309de87591a87b8f39b09c1e6de
umasuthan subramaniam <umavcs@gmail.com>
2015-02-05 14:34:50 +0530

    Grid for order

--- app/views/admin/orderedit.blade.php
+++ app/views/admin/orderedit.blade.php
@@ -34,57 +34,66 @@
         Agent: @{{order.agent}} <br>
         Tabel: @{{order.location}} <br>
         @{{order.description}} <br>
-        <tabset>
-            <tab ng-repeat="category in categorys"
-                 heading="@{{category.name}}" select="getFoodItem(category.id)">
-                <div >
-                    <div style="padding-top:10px;">
-                        <div ng-repeat="item in items" class="label label-primary" style="margin: 5px;
-    padding: 10px; cursor:pointer;" ng-click="addItem(item.id, order.id);" >
-                            @{{item.name}}
-                        </div>
-                    </div>
-                </div>     
-            </tab>
-        </tabset>
-        <div class="row show-grid">
-            <div class="col-xs-12 col-md-8">
+        
+		<div class="row">
+		<div class="col-xs-6">
+			<div class="row show-grid">
+				<div class="col-xs-12 col-md-8">
 
-                <div ng-repeat="orderitem in orderitems">
-                    <div class="col-xs-12 col-md-8">
-                         @{{orderitem.item}} <a class="btn btn-danger" ng-click="deleteItem(orderitem.id)"><i class="fa fa-trash-o"></i></a>
-                    </div>
-                    <div class="col-xs-6 col-md-4">
-                    	<div class="input-group">
-    			          <span class="input-group-btn">
-    			              	<button type="button" class="btn btn-default btn-number" ng-click="nItem(orderitem.id)" data-type="minus" data-field="quant[1]">
-    			                  	<span class="glyphicon glyphicon-minus"></span>
-    			              	</button>
-    			          </span>
-    			          <input type="text" name="quant[1]" class="form-control input-number" ng-model="orderitem.qty" min="1" max="3" >
-    			          <span class="input-group-btn">
-    			              	<button type="button" class="btn btn-default btn-number" ng-click="pItem(orderitem.id)" data-type="plus" data-field="quant[1]">
-    			                  	<span class="glyphicon glyphicon-plus"></span>
-    			              	</button>
-    			          </span>
-    				    </div>
+					<div ng-repeat="orderitem in orderitems">
+						<div class="col-xs-12 col-md-8">
+							 @{{orderitem.item}} <a class="btn btn-danger" ng-click="deleteItem(orderitem.id)"><i class="fa fa-trash-o"></i></a>
+						</div>
+						<div class="col-xs-6 col-md-4">
+							<div class="input-group">
+							  <span class="input-group-btn">
+									<button type="button" class="btn btn-default btn-number" ng-click="nItem(orderitem.id)" data-type="minus" data-field="quant[1]">
+										<span class="glyphicon glyphicon-minus"></span>
+									</button>
+							  </span>
+							  <input type="text" name="quant[1]" class="form-control input-number" ng-model="orderitem.qty" min="1" max="3" >
+							  <span class="input-group-btn">
+									<button type="button" class="btn btn-default btn-number" ng-click="pItem(orderitem.id)" data-type="plus" data-field="quant[1]">
+										<span class="glyphicon glyphicon-plus"></span>
+									</button>
+							  </span>
+							</div>
 
-                    </div>
-                </div>
+						</div>
+					</div>
 
-                <div >
-                    <div class="col-xs-12 col-md-8">
-                         Total:
-                    </div>
-                    <div class="col-xs-6 col-md-4">
+					<div >
+						<div class="col-xs-12 col-md-8">
+							 Total:
+						</div>
+						<div class="col-xs-6 col-md-4">
 
-                        @{{ getTotal() }}
+							@{{ getTotal() }}
 
-                    </div>
-                </div>
+						</div>
+					</div>
 
-            </div>
-        </div>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-6">
+			<tabset>
+				<tab ng-repeat="category in categorys"
+					 heading="@{{category.name}}" select="getFoodItem(category.id)">
+					<div >
+						<div style="padding-top:10px;">
+							<div ng-repeat="item in items" class="label label-primary" style="margin: 5px;
+		padding: 10px; cursor:pointer;" ng-click="addItem(item.id, order.id);" >
+								@{{item.name}}
+							</div>
+						</div>
+					</div>     
+				</tab>
+			</tabset>
+		</div>
+		</div>
+
+        
                 
     	<!-- Content bottom -->
 	@include('admin.include.cont_bot')
