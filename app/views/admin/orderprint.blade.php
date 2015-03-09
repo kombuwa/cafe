018e415f79ce07dddbd24618f87a225460c6555e
umasuthan subramaniam <umavcs@gmail.com>
2015-03-09 14:40:43 +0530

    bill

--- app/views/admin/orderprint.blade.php
+++ app/views/admin/orderprint.blade.php
@@ -32,7 +32,7 @@
             var searchId = {{ $searchId }};
         </script>
         
-        <div style="width:215px;">
+        <div style="width:215px; text-align:center">
         <h3>Cafe Ceylon</h3>
         -------------------<br>
         Matara Road Kabalana, Ahangama, Sri Lanka.</br>
@@ -49,6 +49,52 @@
 						</div>
 					</div>
         </div>
+        <div >
+						<div class="col-xs-12 col-md-8 text-right">
+							 Total:
+						</div>
+						<div class="col-xs-6 col-md-4 text-right">
+
+							@{{ getTotal() }}
+
+						</div>
+					</div>
+                    <div >
+						<div class="col-xs-12 col-md-8 text-right">
+							 Tax:
+						</div>
+						<div class="col-xs-6 col-md-4 text-right">
+
+							@{{ (getTotal()/100)*20 }}
+
+						</div>
+					</div>
+                    <div >
+						<div class="col-xs-12 col-md-8 text-right">
+							 Discount:
+						</div>
+						<div class="col-xs-6 col-md-4 text-right">
+
+							<select ng-model="discount" >
+							  <option value="0" selected="selected">None</option>
+							  <option value="5">5 %</option>
+							  <option value="10">10 %</option>
+							  <option value="15">15 %</option>
+							  <option value="20">20 %</option>
+							</select> 
+
+						</div>
+					</div>
+                    <div >
+						<div class="col-xs-12 col-md-8 text-right">
+							 <strong>To Pay:</strong>
+						</div>
+						<div class="col-xs-6 col-md-4 text-right">
+
+							<strong>@{{ getTotal() + ((getTotal()/100)*20) - ((getTotal()/100)*discount)}}</strong>
+
+						</div>
+					</div>
         
 		<div class="row">
 		<div class="col-md-8">
