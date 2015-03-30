1a49aedcbcdeaec8f2fa04457a9d3aa565dc4571
Isuru Udana Palihawadana <isuruudana@gmail.com>
2015-03-30 10:32:06 +0530

    test update

--- app/views/admin/orderprint.blade.php
+++ app/views/admin/orderprint.blade.php
@@ -31,64 +31,10 @@
         <script type="text/javascript">
             var searchId = {{ $searchId }};
         </script>
+
+        <p>
         @{{invoice.invoice}}
-       <!-- <div style="width:270px; text-align:center; font-size:12px; font-family:Verdana, Geneva, sans-serif">
-        <h3>Cafe Ceylon</h3>
-        ==========================<br>
-        Matara Road Kabalana, <br>
-        Ahangama, Sri Lanka.<br>
-        09 12282729<br>
-        E mail : sales@cafeceylon.lk <br>
-        ==========================<br>
-        Waiter: umavcs | Table:2 | Pax:2<br>
-        ==========================<br>
-<br>
-        </div>
-        <div style="width:270px; text-align:center; font-size:12px; font-family:Verdana, Geneva, sans-serif;">
-            <table width="270" border="0">
-              <tr>
-                <td width="150" align="left">Item</td>
-                <td width="40" align="center">Qty</td>
-                <td width="60" align="right">Amount</td>
-              </tr>
-              <tr  ng-repeat="orderitem in orderitems">
-                <td align="left">@{{orderitem.item}}</td>
-                <td align="center">@{{orderitem.qty}}</td>
-                <td align="right">@{{orderitem.price * orderitem.qty}}</td>
-              </tr>
-              
-            </table>
-        </div>
-        
-        <div style="width:270px; text-align:center; font-size:12px; font-family:Verdana, Geneva, sans-serif;" >
-            ==========================<br>
-        </div>
-        <div style="width:270px; text-align:center; font-size:12px; font-family:Verdana, Geneva, sans-serif;" >
-            <table width="270" border="0">
-              <tr>
-                <td align="left">Gross Amount </td>
-                <td align="right">@{{ getTotal() }}</td>
-              </tr>
-              <tr>
-                <td align="left">Discounts</td>
-                <td align="right">@{{ getTotal() }}</td>
-              </tr>
-              <tr>
-                <td align="left">Net Amount </td>
-                <td align="right">@{{ getTotal() + ((getTotal()/100)*20) - ((getTotal()/100)*discount)}}</td>
-              </tr>
-            </table>
-  </div>
-  
-  <div style="width:270px; text-align:center; font-size:12px; font-family:Verdana, Geneva, sans-serif">
-        
-        ==========================<br>
-        Thank You <br>
-        Visit us: cafeceylon.lk<br>
-        
-       
-<br>
-        </div>-->
+       </p>
         
         
 		
