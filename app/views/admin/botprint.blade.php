b8812274fbc78bc87c6969b1af95703ed00742e0
Isuru Udana Palihawadana <isuruudana@gmail.com>
2015-05-20 12:07:16 +0530

    bot update

---
+++ app/views/admin/botprint.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- Header -->
+	@include('admin.include.head')
+        <script>
+            //Call This At The Top Of Your Layout
+            myApp = new AngularBooter('myApp'); // <-- What do you want to call your ng-app?
+        </script>
+
+</head>
+
+<body ng-cloak ng-app="myApp">
+	<?php 
+		$username = Session::get('current_user');
+		$user_access = Session::get('user_access');
+		$user_id = Session::get('user_id');
+	 ?>
+
+     <!-- Angular -->
+    @include('admin.include.app.botprint')
+
+    <div id="wrapper" ng-controller="orderController">
+
+        <!-- Navigation -->
+	@include('admin.include.nav')
+
+	<!-- Content Top -->
+	@include('admin.include.cont_top')
+        <script type="text/javascript">
+            var searchId = {{ $searchId }};
+        </script>
+
+        <div ng-bind-html="invoice">
+
+       </div>
+        
+        
+		
+
+        
+                
+    	<!-- Content bottom -->
+	@include('admin.include.cont_bot')
+    
+    </div>
+    <!-- /#wrapper -->
+
+    
+    <!-- Footer -->
+	@include('admin.include.foot')
+    <script>
+    myApp.boot();
+    </script>
+
+</body>
+
+</html>
